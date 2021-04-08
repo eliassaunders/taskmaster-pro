b4dc293da0744c5714fed1db289372c319910279
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-08 16:26:49 -0700

    full functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -217,6 +217,21 @@ $(".card .list-group").sortable({
     console.log(tempArr)
   }
 });
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 // load tasks for the first time
 loadTasks();
 
